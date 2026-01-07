5b6fb8fd5cc8c0b414439c29644e2e641681e81b
kngu414 <kngu414@wgu.edu>
2026-01-07 10:48:15 -0800

    Initial project structure

---
+++ BackEnd/WorkTrackerD424/src/main/java/com/wgu/capstone/worktracker/WorkTrackerD424Application.java
@@ -0,0 +1,13 @@
+package com.wgu.capstone.worktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTrackerD424Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTrackerD424Application.class, args);
+    }
+
+}
---
+++ BackEnd/WorkTrackerD424/src/test/java/com/wgu/capstone/worktracker/WorkTrackerD424ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgu.capstone.worktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTrackerD424ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
