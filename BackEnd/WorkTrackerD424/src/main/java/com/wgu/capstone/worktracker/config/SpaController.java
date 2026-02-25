efca8fd82d4197cc90045d68e40e3c7bff7a529b
kngu414 <kngu414@wgu.edu>
2026-02-25 11:02:21 -0800

    Debug.

--- BackEnd/WorkTrackerD424/src/main/java/com/wgu/capstone/worktracker/config/SpaController.java
+++ BackEnd/WorkTrackerD424/src/main/java/com/wgu/capstone/worktracker/config/SpaController.java
@@ -1,4 +1,12 @@
 package com.wgu.capstone.worktracker.config;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SpaController {
+    @GetMapping("/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
 }
